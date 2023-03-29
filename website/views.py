74186c60672f042ec1863e22ded871e21707a0eb
DavidTGAUTIER <davidtiffeneau@live.fr>
2023-03-29 09:37:34 +0200

    add file

--- website/views.py
+++ website/views.py
@@ -61,3 +61,6 @@ def customer_record(request, pk):
         # Look up Records
         customer_record = Record.objects.get(id=pk)
         return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You must be logged in to view that page")
+        return redirect('home')
